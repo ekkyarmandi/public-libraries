67debc846efdc13cb5e29be1c441503229b8f5f1
Ekky Armandi <ekkyarmandi@gmail.com>
2023-06-05 16:33:28 +0800

    added unit django pytest [wip]

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Account
 
-# Create your tests here.
+
+class AccountModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Account.objects.create(
+            fullname="Test User",
+            username="testuser",
+            email="test@mail.com",
+            password="testpassword",
+        )
+
+    def test_account_fullname(self):
+        acc = Account.objects.get(id=1)
+        self.assertEqual(acc.fullname, "Test User")
+
+    def test_account_username(self):
+        acc = Account.objects.get(id=1)
+        self.assertEqual(acc.username, "testuser")
+
+    def test_account_email(self):
+        acc = Account.objects.get(id=1)
+        self.assertEqual(acc.email, "test@mail.com")
--- public_libraries/tests.py
+++ public_libraries/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from accounts.models import Account
+from .models import Book
 
-# Create your tests here.
+class BorrowRetrunBookTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # create book data
+        Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            isbn="123456789",
+            publisher="Test Publisher",
+            number_of_page=123,
+            language="English",
+            stock=3,
+            category="Test Category",
+            image_url="https://testurl.com",
+            description="Test Description",
+        )
+
+        # create user data
+        Account.objects.create(
+            username="testuser",
+            fullname="Test User",
+            email="test@mail.com",
+            password="testpassword",
+        )
+
+    def test_borrow_book_request(self):
+        # login user
+        self.client.login(email="test@mail.com", password="testpassword")
+
+        # get book id
+        book_id = Book.objects.get(title="Test Book").id
+
+        # send borrow request
+        response = self.client.patch(
+            reverse("borrow-book", kwargs={"pk": book_id})
+        )
+
+        # check if borrow request is successful
+        self.assertEqual(response.status_code, 200)
